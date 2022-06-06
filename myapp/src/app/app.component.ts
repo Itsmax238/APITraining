61b58ee4baca0de166284d37c4c5db4eeb838ec9
Max H <itsmax238@comcast.net>
2022-06-05 21:18:22 -0400

    Second Directive, with my name

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 @Component({
 selector: 'app-root',
-template: `<h1>Hello world! {{title}}</h1><second-directive></second-directive>`
+template: `<h1>Hello world! {{title}}</h1>
+<second-directive [firstName]="author"></second-directive>`
 })
 export class AppComponent {
-
 public title = 'This is Angular!';
+public author = "Max.";
 }
